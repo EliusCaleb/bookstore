ff0303895fc74ca504b5587b582dded9e83f03b4
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-30 10:05:40 +0300

    created the searchform

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 import './SearchForm.css'
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
+    <div className='search-form'>
+      <div className='container'>
+        <div className='search-form-content'>
+          <form className='search-form'>
+            <div className='search-form-elem bg-white flex flex-sb'>
+              <input type='text'className='form-control' placeholder=' The Lost city ...'/>
+              <button className='flex flex-c'>
+               <FaSearch className='text-purple' size={32}/>
+              </button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
