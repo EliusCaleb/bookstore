b63988b485a86235d335c99ca9afeb82a3821bce
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-30 11:17:09 +0300

    update the routes in app  components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BookDetails from "./components/BookDetails/BookDetails";
 import Booklist from "./components/Booklist/Booklist";
-import Home  from './pages/Home/Home'
-import About from './pages/About/About'
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 function App() {
   return (
-    <div className="">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="book" element={<Booklist/>} />
-        <Route path="/book/:id" element={<BookDetails/>} />
-      </Routes>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="about" element={<About />} />
+        <Route path="book" element={<Booklist />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+      </Route>
+    </Routes>
   );
 }
 
